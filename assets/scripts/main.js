a0fad943d7ed586f8452a911b614c649ceeaf940
Ayleus <30358351+Ayleus@users.noreply.github.com>
2018-06-03 18:39:59 +0100

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@
 	function removeHiddenCharacters (evt) {
 		document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML = document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML.replace(/\u200b/g, '');
 		document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML = document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML.replace(/\u00a0/g, ' ');
+		document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML = document.getElementsByClassName("ms-rte-layoutszone-inner")[0].innerHTML.replace(/%E2%80%8B/g, '');
 	}
 	window.addEventListener("load", removeHiddenCharacters, false);
 })();
